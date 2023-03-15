35e10ce6bcd149f60d018985b83bc482322ce04f
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2023-03-15 21:21:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+/***** sidebar navigation  */
+const sidebarNavigationEl = document.getElementById(
+  "sidebar-container-navigation-id"
+);
+const sidebarOpenNavigationEl = document.getElementById("open-nav-sidebar");
+const sidebarCloseNavigationEl = document.getElementById(
+  "sidebar-navigation-close"
+);
+
+//  console.log(sidebarNavigationEl)
+
+sidebarOpenNavigationEl.addEventListener("click", () => {
+  sidebarNavigationEl.classList.toggle("slidebar-show");
+});
+sidebarCloseNavigationEl.addEventListener("click", () => {
+  sidebarNavigationEl.classList.toggle("slidebar-show");
+});
