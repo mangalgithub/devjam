59366b8ddd1aebc095079969618e66faaf72ebd9
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2023-03-19 13:56:38 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,22 @@
 
+const conter = document.querySelector('.conter');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+const btnpop = document.querySelector('.loginbtn');
+const icncls = document.querySelector('.icon-close');
+
+registerlink.addEventListener('click', ()=> {
+    conter.classList.add('active');
+});
+
+loginlink.addEventListener('click', ()=> {
+    conter.classList.remove('active');
+});
+
+btnpop.addEventListener('click', ()=> {
+    conter.classList.add('active2');
+});
+
+icncls.addEventListener('click', ()=> {
+    conter.classList.remove('active2');
+});
